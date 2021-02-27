a60aa6a867fa12d94aefccbf2520f2dcea6ed9df
Jack Funck <jackfunck21@gmail.com>
2021-02-27 16:39:43 -0500

    sdfg

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const express = require("express");
+
+const connection = ({
+    host: "localhost",
+
+    port: 3306,
+
+    password: "1234567",
+
+    database: "employees_db"
+
+});
 
 
-const app = express();
+
