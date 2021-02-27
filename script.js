decd2a0291bef4b0f4f45d51ba2c77cf961b9d8e
Jack Funck <jackfunck21@gmail.com>
2021-02-27 16:55:35 -0500

    got connection established

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
-const connection = ({
+const connection = mysql.createConnection({
     host: "localhost",
 
     port: 3306,
+
+    user: "root",
 
     password: "1234567",
 
@@ -12,10 +14,10 @@ const connection = ({
 
 });
 
+
+
 connection.connect((err) => {
-    if (err) {
-        throw err;
-    }
+    if (err) throw err;
     console.log(`connected as id ${connection.threadId}`);
 });
 
