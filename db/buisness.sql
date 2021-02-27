ead671c905f0ad2bc42946f875f1a71c6632a188
Jack Funck <jackfunck21@gmail.com>
2021-02-27 16:29:49 -0500

    built the tables for the database

---
+++ db/buisness.sql
@@ -0,0 +1,27 @@
+drop database if exists employees_db;
+
+create database employees_db;
+
+use employees_db;
+
+create table department (
+id int not null auto_increment,
+name varchar(50) not null,
+primary key(id)
+);
+
+create table  role (
+id int not null auto_increment,
+title varchar(50) not null,
+salary decimal (9,2) not null,
+department_id int not null,
+primary key(id)
+);
+
+create table employee (
+id int not null auto_increment,
+first_name varchar(50) not null,
+last_name varchar(50) not null,
+role_id int not null,
+primary key(id)
+);
