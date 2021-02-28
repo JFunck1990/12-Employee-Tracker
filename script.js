09ffda3fc91d964958bb716c64324e25729d0025
Jack Funck <jackfunck21@gmail.com>
2021-02-28 11:59:36 -0500

    added the if statements

--- script.js
+++ script.js
@@ -16,8 +16,47 @@ console.log( "This is questions: "+ questions.main.choose);
 
 function qStart () {
 inquirer.prompt(questions.main).then(res => {
-switch (res.main) {
+ if(res.choose === "View all employees"){
 
+ }
+if(res.choose === "View all employees by role") {
+
+}
+if(res.choose === "View all employees by department"){
+
+}
+
+if(res.choose === "View all employees by manager") {
+
+}
+if(res.choose === "Add employee"){
+
+}
+if(res.choose === "Add role"){
+
+}
+if(res.choose === "Add department"){
+
+}
+if(res.choose === "Update employee role"){
+
+}
+if(res.choose === "Update employee manager"){
+
+}
+if (res.choose === "Delete employee"){
+
+}
+if(res.choose === "Delete role"){
+
+}
+if(res.choose === "Delete department"){
+
+}
+if(res.choose ===  "View department budgets"){
+
+}
+if(res.choose === "Exit"){
 
 }
 
